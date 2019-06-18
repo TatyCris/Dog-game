4edf83c3a68eee8241424736bd5fc4ac865141c4
TATY <tatyccsilva@gmail.com>
2019-06-18 17:10:28 +0200

    Implementing game logic function - game1

--- src/components/GameOneContainer.js
+++ src/components/GameOneContainer.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
 import GameOne from './GameOne';
+import { connect } from 'react-redux'
 
-export default class Game1Container extends Component {
+
+class Game1Container extends Component {
+
+    chooseRamdomBreed = (breeds) => {
+        return breeds[Math.floor(Math.random() * breeds.length)]
+    }
+
+    getAnswers = (breeds) => {
+        const randomAnswer1 = this.chooseRamdomBreed(breeds)
+        let randomAnswer2 = this.chooseRamdomBreed(breeds)
+        let correctAnswer = this.chooseRamdomBreed(breeds)
+
+        while (randomAnswer1 === randomAnswer2) {
+            randomAnswer2 = this.chooseRamdomBreed(breeds)
+        }
+        while (randomAnswer2 === correctAnswer) {
+            correctAnswer = this.chooseRamdomBreed(breeds)
+        }
+        while (randomAnswer1 === correctAnswer) {
+            correctAnswer = this.chooseRamdomBreed(breeds)
+        }
+
+        const answers = [randomAnswer1, randomAnswer2, correctAnswer]
+        const imageQuestion = breeds.find(breed => breed === answers[2])
+        console.log('image', imageQuestion);
+
+        return answers
+    }
+
     render() {
         return (
             <div>
@@ -10,3 +39,5 @@ export default class Game1Container extends Component {
         )
     }
 }
+
+export default connect()(Game1Container)
