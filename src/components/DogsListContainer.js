ee6aeb7a0b6e14b38459477b3714b4bd1e105409
mnezmah <mario.nezmah@gmail.com>
2019-06-18 15:16:55 +0200

    added title to first component

--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -3,6 +3,7 @@ import DogsList from './DogsList';
 import { connect } from 'react-redux'
 import { getBreeds } from '../actions/breeds'
 import Button from './Button'
+import Title from './Title'
 
 class DogsListContainer extends Component {
 
@@ -14,7 +15,7 @@ class DogsListContainer extends Component {
         return (
 
             <div>
-                <h1>Hello from DogsList component :)</h1>   {/* Replace this with a title component */}
+                <Title content="Wello from DogsList component :)"/>
                 <Button title="Play game 1" onclick="" />
                 <Button title="Play game 2" onclick="" />
                 <Button title="Play game 3" onclick="" />
--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Image from './Image'
 import Button from './Button'
+import Title from './Title'
+
 
 export default class GameOne extends Component {
   render() {
     return (
       <div>
-        <h1>Hello from Single Dog component :)</h1>   {/* Replace this with a title component */}
+        <Title content="Wellcome to game one"/>
         <Image ulr="#" alt="speciFicDogBreed"></Image>
         <Button title="first random name" onClick=""/>
         <Button title="second random name" onClick=""/>
