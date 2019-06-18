f16384a7adc6a12ac67f5aa83e019d30ef400b01
mnezmah <mario.nezmah@gmail.com>
2019-06-18 17:35:16 +0200

    routed game2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DogsListContainer from './components/DogsListContainer';
 import DogBreedImagesContainer from './components/DogBreedImagesContainer';
 import { Route } from 'react-router-dom'
 import Game1Container from './components/GameOneContainer'
+import GameTwoContainer from './components/GameOneContainer'
 
 
 
@@ -16,6 +17,7 @@ export default class App extends Component {
           <Route exact path="/" component={DogsListContainer} />
           <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
           <Route exact path="/game1" component={Game1Container} />
+          <Route exact path="/game2" component={GameTwoContainer} />
           {/* <Route exact path="/game2" component={Game2Container} />
           <Route exact path="/game3" component={Game3Container} /> */}
         </header>
--- src/components/GameTwoContainer.js
+++ src/components/GameTwoContainer.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
-import Title from './Title'
 import GameTwo from './GameTwo'
-export default class GameTwoContainer extends Component {
+import { connect } from 'react-redux'
+
+
+ class GameTwoContainer extends Component {
+
+    chooseRamdomBreed = (breeds) => {
+        return breeds[Math.floor(Math.random() * breeds.length)]
+    }
+    getImages = (breeds) => {
+        const randomImage1 = this.chooseRamdomBreed(breeds)
+        let randomImage2 = this.chooseRamdomBreed(breeds)
+        let correctImage = this.chooseRamdomBreed(breeds)
+
+        while (randomImage1 === randomImage2) {
+            randomImage2 = this.chooseRamdomBreed(breeds)
+        }
+        while (randomImage2 === correctImage) {
+            correctImage = this.chooseRamdomBreed(breeds)
+        }
+        while (randomImage1 === correctImage) {
+            correctImage = this.chooseRamdomBreed(breeds)
+        }
+
+        const images = [randomImage1, randomImage2, correctImage]
+        const nameQuestion = breeds.find(breed => breed === images[2])
+        console.log('image', nameQuestion);
+
+        return images
+
+    }
     render() {
         return (
             <div>
-            
             <GameTwo />
             </div>
         )
     }
 }
+
+export default connect()(GameTwoContainer)
