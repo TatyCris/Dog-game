c39f581dc70fa1ba9569c9afb0d592e44b83da76
mnezmah <mario.nezmah@gmail.com>
2019-06-18 14:25:46 +0200

    added Button component

--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import DogsList from './DogsList';
 import { connect } from 'react-redux'
 import { getBreeds } from '../actions/breeds'
-import Button from './'
+import Button from './Button'
 
 class DogsListContainer extends Component {
-    static propTypes = {
-        content: propTypes.string.isRequired,
-      }
+
     componentDidMount() {
         this.props.getBreeds()
     }
@@ -15,6 +13,9 @@ class DogsListContainer extends Component {
     render() {
         return (
             <div>
+                <Button onclick="">Play game 1</Button>
+                <Button onclick="">Play game 2</Button>
+                <Button onclick="">Play game 3</Button>
                 <DogsList dogBreeds={this.props.dogBreeds} />
             </div>
         )
