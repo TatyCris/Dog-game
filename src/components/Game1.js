8e3e9e085e63ad1817ccefdca8c903e2bf2a3292
TATY <tatyccsilva@gmail.com>
2019-06-18 15:09:52 +0200

    Create game1 logic - vanilla js

---
+++ src/components/Game1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Game1 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
