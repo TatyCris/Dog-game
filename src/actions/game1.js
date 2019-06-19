ec8d9719bdc88ffa890204f62a68e3643fbed89b
TATY <tatyccsilva@gmail.com>
2019-06-19 16:12:32 +0200

    Make a status for game1

---
+++ src/actions/game1.js
@@ -0,0 +1,8 @@
+export const NEXT_QUESTION = 'NEXT_QUESTION'
+
+export const nextQuestion = () => {
+    return {
+        type: NEXT_QUESTION,
+        payload: []
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import dogs from './dogs'
+import game1 from './game1'
 
 export default combineReducers({
-    dogs
+    dogs,
+    game1
 }) 
