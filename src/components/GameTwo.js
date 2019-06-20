f328302ffc2855fbcf986d08398aa758c6fe6db2
TATY <tatyccsilva@gmail.com>
2019-06-20 12:33:51 +0200

    Work in game 2

--- src/components/GameTwo.js
+++ src/components/GameTwo.js
@@ -2,39 +2,34 @@ import React from 'react'
 import Image from './Image'
 import Score from './Score'
 import Title from './Title'
-import { Link } from 'react-router-dom'
-import Button from './Button'
+// import { Link } from 'react-router-dom'
+// import Button from './Button'
 
-export default function GameTwo (props) {
+export default function GameTwo(props) {
+  const images = props.answers[0]
 
-<<<<<<< HEAD
-    return (
-      <div>
-        {/* {console.log(props, 'PROPS')} */}
-        <Title content="Welcome to game Two" />
-        <h3>{props.title}</h3>
-        {/* {dataDisplay(props.image)} */}
-        <Image url={props.image} alt="randomDogBreed" onclick=''></Image>
-        <Image url={props.image} alt="randomDogBreed" onclick=''></Image>
-        <Image url={props.image} alt="randomDogBreed" onclick=''></Image>
-        
-       
-
-
-        
-=======
-  console.log(props.image)
-    return (
-      <div>
-        {/* <Link to={`/`}><Button title="go to Home Page" onclick={this.props.clicked} /></Link> */}
-        <Title content="Welcome to game Two" />
-        <h3>{props.title}</h3>
-        {/* {dataDisplay(props.image)} */}
-        { !props.image && 'Loading...' } 
-        { props.image &&  <Image url={props.image} alt="randomDogBreed" onclick=''></Image>}    
->>>>>>> 81c96bdc5ca40410ea488c3e0ee944b676a235fe
-        <Score />
-      </div>
-    )
   
+  return (
+    <div>
+      {console.log('props', props.answers)}
+      {console.log('answers', props.answers[0])}
+      
+      <Title content="Welcome to game Two" />
+      <h3>{props.title}</h3>
+      <div>
+        { images && images.map((url, index) => <Image breed={ url } alt="Dog" key={index}/>) }
+        { !images && 'Loading...' } 
+      </div>
+      <Image breed={'akita'} num={1} />
+      {/* <Image url={props.image} alt="randomDogBreed" onclick=''></Image>
+        <Image url={props.image} alt="randomDogBreed" onclick=''></Image> */}
+
+
+
+
+
+      <Score />
+    </div>
+  )
+
 }
