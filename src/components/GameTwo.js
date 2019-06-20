7e09282921bf9147a55987b309ab1b71b2455f0a
TATY <tatyccsilva@gmail.com>
2019-06-20 12:36:04 +0200

    commit to merge

--- src/components/GameTwo.js
+++ src/components/GameTwo.js
@@ -7,7 +7,6 @@ import Button from './Button'
 
 export default function GameTwo (props) {
 
-<<<<<<< HEAD
     return (
       <div>
         {/* {console.log(props, 'PROPS')} */}
@@ -22,17 +21,6 @@ export default function GameTwo (props) {
 
 
         
-=======
-  console.log(props.image)
-    return (
-      <div>
-        {/* <Link to={`/`}><Button title="go to Home Page" onclick={this.props.clicked} /></Link> */}
-        <Title content="Welcome to game Two" />
-        <h3>{props.title}</h3>
-        {/* {dataDisplay(props.image)} */}
-        { !props.image && 'Loading...' } 
-        { props.image &&  <Image url={props.image} alt="randomDogBreed" onclick=''></Image>}    
->>>>>>> 81c96bdc5ca40410ea488c3e0ee944b676a235fe
         <Score />
       </div>
     )
