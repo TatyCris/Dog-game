a7b1c59db15739bcbbddcd65f5fa0209ecf4559d
Astrid88 <giu.munari@gmail.com>
2019-06-20 16:19:37 +0200

    works something!

--- src/actions/randomBreed.js
+++ src/actions/randomBreed.js
@@ -4,8 +4,11 @@ export const RANDOM_BREED = 'RANDOM_BREED'
 
 function chooseRandomBreed(breeds) {
     const myBreed = breeds[Math.floor(Math.random() * breeds.length)]
-    return myBreed
+    const mySecondBreed = breeds[Math.floor(Math.random() * breeds.length)]
+    const myThirdBreed = breeds[Math.floor(Math.random() * breeds.length)]
+    return [myBreed, mySecondBreed, myThirdBreed]
 }
+
 
 function setRandomBreed (payload) {
     return {
@@ -24,24 +27,53 @@ function getRandomImage (breed) {
         })
 }
 
+// export function getRandomBreed() {
+//     return function (dispatch) {
+//         request('https://dog.ceo/api/breeds/list/all')
+//             .then(response => {
+//                 const { message } = response.body
+//                 //const breeds = Object.keys(message)
+//                 //const breed = chooseRandomBreed(breeds)
+//                 //const promise = getRandomImage(breed)
+//                 const breeds = ['akita', 'maltese', 'akita']
+//                 const promises = ['akita', 'maltese', 'akita'].map(breed=>getRandomImage(breed))
+//                 return Promise.all(promises).then(imgurls => ({ imgurls, breeds }))
+//             })
+//             .then(data => {
+//                 console.log(data.imgurls, data.breeds, 'data from promise')
+//                 const action = setRandomBreed(data)
+//                 dispatch(action)
+//             })
+//             .catch(console.error)
+//     }
+// } 
+
 export function getRandomBreed() {
-    return function (dispatch) {
-        request('https://dog.ceo/api/breeds/list/all')
-            .then(response => {
-                const { message } = response.body
-                //const breeds = Object.keys(message)
-                //const breed = chooseRandomBreed(breeds)
-                //const promise = getRandomImage(breed)
-                const breeds = ['akita', 'maltese', 'akita']
-                const promise = message.map(breed=>getRandomImage(breed))
-                const myData = [promise, breeds]
-                console.log(myData, 'myData')
-                return myData
-            })
-            .then(data => {
-                const action = setRandomBreed(data)
-                dispatch(action)
-            })
-            .catch(console.error)
+    return async function (dispatch) {
+        try {
+            const response = await request('https://dog.ceo/api/breeds/list/all')
+            const { message } = response.body
+            const breed = Object.keys(message)
+            console.log(breed.length)
+            console.log(chooseRandomBreed(breed))
+            const imgUrls = await Promise.all(['akita', 'maltese', 'akita'].map(breed=>getRandomImage(breed)))
+            console.log(imgUrls)
+            dispatch() 
+        } catch(error) {
+            console.error(error)
+        }
+            // .then(response => {
+            //     //const breeds = Object.keys(message)
+            //     //const breed = chooseRandomBreed(breeds)
+            //     //const promise = getRandomImage(breed)
+            //     const breeds = ['akita', 'maltese', 'akita']
+            //     return Promise.all(promises).then(imgurls => ({ imgurls, breeds }))
+            // })
+            // .then(data => {
+            //     console.log(data.imgurls, data.breeds, 'data from promise')
+            //     const action = setRandomBreed(data)
+            //     dispatch(action)
+            // })
+            // .catch(console.error)
     }
 } 
