f4c6b54360a6474995fbd6fe15dc613d2c325b40
Astrid88 <giu.munari@gmail.com>
2019-06-21 16:02:22 +0200

    my commit

--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -6,17 +6,8 @@ import Score from './Score'
 import { Link } from 'react-router-dom'
 
 export default class GameOne extends Component {
-  // getImage = () => {
-  //   return <Image breed={[images[2]]} num={1} />
-  // }
-
   render() {
-
-    console.log('props', this.props.answers);
-
     const images = this.props.answers
-    console.log('images', images);
-
     const mixedAnswers = this.props.mixAnswers(this.props.answers)
     const gameCondition = this.props.score < 10
     return (
@@ -34,15 +25,14 @@ export default class GameOne extends Component {
         <br />
         <Button title={mixedAnswers[0]} checkAnswer={this.props.checkAnswer} />
         <Button title={mixedAnswers[1]} checkAnswer={this.props.checkAnswer} />
-       
+        <Button title={mixedAnswers[2]} checkAnswer={this.props.checkAnswer}  />
+        </div>
         }
-
-        <Button title={mixedAnswers[2]} checkAnswer={this.props.checkAnswer} />
         <Score size={this.props.score} />
         {!gameCondition &&
           <div>
             <Link to={`/game1`}><Button title="Play game 1" /></Link>
-            <Link to={`/game2`}><Button title="Play game 2" /></Link>
+            <Link to={`/game2`}><Button title="Play game 3" /></Link>
           </div>
         }
         <Link to={`/`}><Button title="I need more practice!" onclick={this.clicked} /></Link>
