0cb53dcb5e242b639ac2e81b1318da628b002a0b
TATY <tatyccsilva@gmail.com>
2019-06-21 14:49:54 +0200

    Add Loading status and store

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -5,16 +5,16 @@ import Button from './Button'
 import './DogBreedImages.css'
 
 export default function DogBreedImages(props) {
-  const images = props.images
+  console.log('DogBreedImages props test:', props)
+  const { images } = props
 
   return (
-    <div className="dog-breed-images">
-      <h1>Meet {props.title}</h1>
-      <Link to={`/`}><Button title="Go back" /></Link>
-      <div className="images-container">
-        {images && <Image breed={[props.breed]} num={10} />}
-        {!images && 'Loading...'}
+      <div className="dog-breed-images">
+        <h1>Meet {props.title}</h1>
+        <Link to={`/`}><Button title="Go back" /></Link>
+        <div className="images-container">
+          {images && <Image breed={[props.breed]} num={10} />}
+        </div>
       </div>
-    </div>
-  )
+    )
 }
--- src/components/DogBreedImagesContainer.js
+++ src/components/DogBreedImagesContainer.js
@@ -6,25 +6,30 @@ import { getImages } from '../actions/images'
 class DogBreedImagesContainer extends Component {
 
   componentDidMount() {
+    console.log('DogBreedImagesContainer componentDidMount test')
     const breed = this.props.match.params.breed
-    this.props.getImages([breed], 10)
+    this.props.getImages([breed], 10, true)
   }
 
 
   render() {
     const breed = this.props.match.params.breed
-    return (
+    return !this.props.loading ? (
       <div>
         <DogBreedImages
-          images={this.props.dogBreeds} title={breed} breed={breed} />
+          images={this.props.dogBreeds}
+          title={breed}
+          breed={breed}
+        />
       </div>
-    )
+    ) : 'Loading...'
   }
 }
 
 const mapStatetoProps = (state) => {
   return {
     dogBreeds: state.dogs.images,
+    loading: state.dogs.loading
   }
 }
 
