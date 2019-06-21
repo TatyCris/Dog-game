d5ff20c27c95b209c0ba4f42608088c449779a8a
TATY <tatyccsilva@gmail.com>
2019-06-21 11:32:12 +0200

    Score working on game3

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import dogs from './dogs'
 import game1 from './game1'
 import game2 from './game2'
+import game3 from './game3'
 import randomBreeds from './randomBreeds'
 
 export default combineReducers({
     dogs,
     game1,
     game2,
+    game3,
     randomBreeds
 }) 
