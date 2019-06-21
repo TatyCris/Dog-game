ca18b74f1c89e4153349c980a25bc248c9a771fb
Astrid88 <giu.munari@gmail.com>
2019-06-21 14:24:07 +0200

    commit

--- src/components/Score.js
+++ src/components/Score.js
@@ -5,15 +5,17 @@ import './Score.css';
 
 export default class Score extends Component {
     render() {
-        const sizeBar = this.props.size
+        const sizeBar = this.props.size * 10 + '%'
         const divWidth = {
-            width: sizeBar * 10 + '%'
-
+            width: sizeBar
         };
         return (
-            <div className='scoreBarContainer'>
-                <div className='scoreBar'>
-                    <div className='progress' style={divWidth} ></div>
+            <div>
+                <h4>Score: {sizeBar}</h4>
+                <div className='scoreBarContainer'>
+                    <div className='scoreBar'>
+                        <div className='progress' style={divWidth} ></div>
+                    </div>
                 </div>
             </div>
         )
